db11b3356c2e9d07e3797c0937e38cab6922d8a3
JuanSebastian10 <49922725+JuanSebastian10@users.noreply.github.com>
2020-10-14 17:09:17 -0500

    Arreglo a CreateView

--- appconsultaeps/views.py
+++ appconsultaeps/views.py
@@ -1,11 +1,27 @@
 #django
 
 from django.shortcuts import render
+<<<<<<< Updated upstream
 from django.views.generic import FormView
 from django.urls import reverse_lazy
 
 #Forms
 from .forms import PacienteForm
+=======
+from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView,CreateView
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .Forms import CitasView
+
+class CrearCitas(CreateView):
+    Model = 'Citas'
+    template_name = 'SenaSoftPython/Citas.html'
+    form_class=CitasView
+
+>>>>>>> Stashed changes
 
 #Models
 from .models import pacientes
